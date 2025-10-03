5032adf8914e0c5e6d6c39cc0b3d7f688437c9db
Jared <jaredlll08@gmail.com>
2025-10-03 17:32:05 -0600

    Port to 1.21.9. Close #164

--- forge/src/main/java/com/blamejared/clumps/Clumps.java
+++ forge/src/main/java/com/blamejared/clumps/Clumps.java
@@ -9,7 +9,7 @@ public class Clumps {
     
     public Clumps() {
         
-        ClumpsCommon.pickupXPEvent = (player, experienceOrb) -> PlayerXpEvent.BUS.post(new PlayerXpEvent.PickupXp(player, experienceOrb));
+        ClumpsCommon.pickupXPEvent = (player, experienceOrb) -> PlayerXpEvent.PickupXp.BUS.post(new PlayerXpEvent.PickupXp(player, experienceOrb));
     }
     
 }
